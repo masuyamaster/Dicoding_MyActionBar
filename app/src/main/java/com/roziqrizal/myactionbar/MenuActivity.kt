1c61e168fd40aa69c88976b35c95b48bea851923
masuyamaster <roziq@masuya.id>
2021-11-14 12:55:16 +0700

    Latihan Option Menu

--- app/src/main/java/com/roziqrizal/myactionbar/MenuActivity.kt
+++ app/src/main/java/com/roziqrizal/myactionbar/MenuActivity.kt
@@ -10,7 +10,8 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        //typically
     }
 
-    
+
 }

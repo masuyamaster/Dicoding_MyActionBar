5e071034d7b539f962d6956dc272246763e2c3bb
masuyamaster <roziq@masuya.id>
2021-11-14 12:53:54 +0700

    firxt commit

---
+++ app/src/main/java/com/roziqrizal/myactionbar/MenuActivity.kt
@@ -0,0 +1,16 @@
+package com.roziqrizal.myactionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    
+}
